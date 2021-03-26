45d34451a161519d0362b1e93ae1a23a0d05a97e
powerferren@gmail.com <powerferren@gmail.com>
2021-03-26 11:32:10 +0200

    Created views

---
+++ views/v_index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <a class="btn btn-info" href="add.php" role="button">Add user</a>
+    <table class="table table-bordered table-hover">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Create Date</th>
+            <th scope="col">Update Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        <? foreach ($users as $user): ?>
+            <tr>
+                <th scope="row"><?=$user['id']?></th>
+                <td><?=$user['first_name']?></td>
+                <td><?=$user['last_name']?></td>
+                <td><?=$user['email']?></td>
+                <td><?=$user['create_date']?></td>
+                <td><?=$user['update_date']?></td>
+            </tr>
+        <? endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="js/bootstrap.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
