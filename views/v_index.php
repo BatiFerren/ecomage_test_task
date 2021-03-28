25963fad6de85d15695f3a6162a16d28d3b6e0f4
BatiFerren <powerferren@gmail.com>
2021-03-28 13:26:13 +0300

    Added paging of table

--- views/v_index.php
+++ views/v_index.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/datetable.css">
     <title>Main Page</title>
 </head>
 <body>
 
 <div class="container">
     <a id="addUserButton" class="btn btn-info" href="add.php" role="button">Add user</a>
-    <table class="table table-bordered table-hover">
+    <table class="table table-bordered table-hover" id="myTable">
         <thead class="thead-dark">
         <tr>
             <th scope="col">ID</th>
@@ -71,8 +72,14 @@
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="js/bootstrap.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+<script src="js/jquery.js"></script>
+<script src="js/datetable.js"></script>
+<script>
+    $(document).ready( function () {
+        $('#myTable').DataTable({
+            searching: false
+        });
+    } );
+</script>
 </body>
 </html>
